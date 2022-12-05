d848b4c72aa4504badd46476ecb3af619feebdce
KASIM İSLAM TATLI <202802055@ogr.cbu.edu.tr>
2022-12-05 17:49:28 +0300

    start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { CarComponent } from './components/car/car.component';
+import { BrandComponent } from './components/brand/brand.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaviComponent,
+    CarComponent,
+    BrandComponent
   ],
   imports: [
     BrowserModule,
